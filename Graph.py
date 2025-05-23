2e40ee678c8f85c973c703aa17084f7aeee83c78
KhaPy39 <k.dahmani@live.fr>
2025-05-23 11:35:02 +0200

    maj Graph.py

--- Graph.py
+++ Graph.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import plotly.express as px
 
 st.title("Manipulation de données et création de graphiques")
